eaf75e7d69eb27d0e97e6f4ad35e400982fab6a4
patoglu <ypatoglu@icloud.com>
2021-09-26 12:31:52 +0300

    i am rich

---
+++ I Am Rich!/I Am Rich!/I_Am_Rich_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_App.swift
+//  I Am Rich!
+//
+//  Created by Yusuf Patoglu on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
