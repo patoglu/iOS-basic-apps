1b6172044434599ee8dc689b188b8fb84984769d
Yusuf Patoglu <ypatoglu@cisco.com>
2022-01-21 18:58:30 +0300

    HullPizza!

---
+++ HullPizza/Shared/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by ypatoglu on 21.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("Huli Pizza Company")
+            Text("Order Pizza")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
