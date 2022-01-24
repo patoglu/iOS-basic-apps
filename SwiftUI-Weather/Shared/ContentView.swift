caa241e99f464e84a7001ccb2a368e36ae0e6c51
Yusuf Patoglu <ypatoglu@cisco.com>
2022-01-24 20:28:36 +0300

    binding view

--- SwiftUI-Weather/Shared/ContentView.swift
+++ SwiftUI-Weather/Shared/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    /*
+     TODO: Learn in depth:
+        -@State
+        -@Binding
+        -@StateObject
+        -@ObservedObject
+        -@EnvironmentObject
+     */
+    
+    @State private var isNight = false
     var body: some View {
         ZStack {
-            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+            BackgroundView(isNight: $isNight)
             VStack {
                 CityTextView(cityName: "Cupertino, CA")
-                MainWeatherStatusView(imageName: "cloud.sun.fill",                          temperature: 76)
+                MainWeatherStatusView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill",                          temperature: 76)
                 HStack (spacing: 20) {
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 74)
                     WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
@@ -25,7 +36,7 @@ struct ContentView: View {
                 Spacer()
                  
                 Button {
-                    print("tapped")
+                    isNight.toggle()
                 } label: {
                     WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
                 }
@@ -67,11 +78,10 @@ struct WeatherDayView: View {
 }
 
 struct BackgroundView: View {
-    var topColor: Color
-    var bottomColor: Color
+    @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
     }
 }
